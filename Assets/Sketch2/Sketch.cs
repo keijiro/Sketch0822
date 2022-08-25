49e6c2c8d9a4b834cec3447c64f5ed421d79cf6a
Keijiro Takahashi <keijiro@gmail.com>
2022-08-25 23:28:54 +0900

    Settings

--- Assets/Sketch2/Sketch.cs
+++ Assets/Sketch2/Sketch.cs
@@ -7,8 +7,11 @@ namespace Sketch2 {
 
 sealed class Sketch : MonoBehaviour
 {
-    [SerializeField] Mesh _mesh = null;
-    [SerializeField] Material _material = null;
+    [SerializeField] Mesh _mesh1 = null;
+    [SerializeField] Mesh _mesh2 = null;
+    [SerializeField] Material _material1 = null;
+    [SerializeField] Material _material2 = null;
+    [SerializeField] float _ratio = 0.5f;
     [SerializeField] uint2 _dimensions = math.uint2(10, 10);
     [SerializeField] float _displacement = 1;
     [SerializeField] float _steepness = 1;
@@ -32,14 +35,16 @@ sealed class Sketch : MonoBehaviour
                 var z = 1 / (1 + math.length(math.float2(x, y)) * _steepness);
                 z *= _displacement * rand.NextFloat(0.3f, 1f);
 
+                var alt = rand.NextFloat() > _ratio;
+
                 var go = new GameObject("Box", comps);
                 go.transform.parent = transform;
                 go.transform.localPosition = new Vector3(x, y, z);
                 go.transform.localRotation = Quaternion.identity;
                 go.transform.localScale = Vector3.one;
 
-                go.GetComponent<MeshFilter>().mesh = _mesh;
-                go.GetComponent<MeshRenderer>().material = _material;
+                go.GetComponent<MeshFilter>().mesh = alt ? _mesh2 : _mesh1;
+                go.GetComponent<MeshRenderer>().material = alt ? _material2 : _material1;
             }
         }
     }
