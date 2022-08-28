123b9a73b3aab2c22168ac075e0a2502356e5dd4
Keijiro Takahashi <keijiro@gmail.com>
2022-08-29 01:01:51 +0900

    temp

--- Assets/Sketch4/Sketch.cs
+++ Assets/Sketch4/Sketch.cs
@@ -1,26 +1,42 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Random = Unity.Mathematics.Random;
 
 namespace Sketch4 {
 
 sealed class Strands : MonoBehaviour
 {
+    [SerializeField] int _poleCount = 5;
+    [SerializeField] float _baseRange = 10;
+    [SerializeField] int _nodeCount = 5;
+    [SerializeField] float _nodeStride = 0.1f;
     [SerializeField] Mesh _shape = null;
+    [SerializeField] uint _seed = 1234;
 
     Mesh _mesh;
 
     void Start()
     {
         var shape = new GeometryCache(_shape);
-
         var stack = new Stack<Modeler>();
 
-        for (var i = 0; i < 10; i++)
+        var rand = new Random(_seed);
+        rand.NextUInt4();
+
+        for (var i = 0; i < _poleCount; i++)
         {
-            var m = new Modeler()
-              { Position = Vector3.forward * 0.1f * i,
-                Rotation = 30 * i, Shape = shape };
-            stack.Push(m);
+            var pos = math.float3(rand.NextFloat2(-0.5f, 0.5f) * _baseRange, 0);
+
+            for (var j = 0; j < _nodeCount; j++)
+            {
+                var m = new Modeler()
+                  { Position = pos, Rotation = 30 * j, Shape = shape };
+
+                stack.Push(m);
+
+                pos += math.float3(0, 0, _nodeStride);
+            }
         }
 
         GetComponent<MeshFilter>().sharedMesh = _mesh = MeshBuilder.Build(stack);
