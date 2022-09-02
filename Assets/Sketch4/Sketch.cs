ae9af89e1718f7db491c5b55f35c0e68b1ecc1d8
Keijiro Takahashi <keijiro@gmail.com>
2022-09-03 01:06:53 +0900

    Refactoring

--- Assets/Sketch4/Sketch.cs
+++ Assets/Sketch4/Sketch.cs
@@ -11,48 +11,65 @@ sealed class Strands : MonoBehaviour
     [SerializeField] float _baseRange = 10;
     [SerializeField] float _decay = 0.9f;
     [SerializeField] float _nodeStride = 0.1f;
-    [SerializeField] Mesh _shape = null;
+    [SerializeField] Mesh _boardMesh = null;
+    [SerializeField] Mesh _poleMesh = null;
     [SerializeField] uint _seed = 1234;
 
     Mesh _mesh;
 
     void Start()
     {
-        var shape = new GeometryCache(_shape);
-        var stack = new Stack<Modeler>();
+        // Meshes
+        var board = new GeometryCache(_boardMesh);
+        var pole = new GeometryCache(_poleMesh);
 
-        var rand = new Random(_seed);
-        rand.NextUInt4();
+        // PRNG
+        var (hash, seed) = (new XXHash(_seed), 0u);
 
+        // Modeler stack
+        var modeling = new Stack<Modeler>();
+
+        // Pole population
         for (var i = 0; i < _poleCount; i++)
         {
-            var p0 = math.float3(rand.NextFloat2(-0.5f, 0.5f) * _baseRange, 0);
-            var prob = 1.0f;
-            var decay = rand.NextFloat(0.1f, 1.0f) * _decay;
-            var diag = rand.NextFloat() < 0.5f;
+            // Position / angle
+            var pos = math.float3(hash.InCircle(seed++) * _baseRange, 0);
+            var angle = hash.Bool(seed++) ? 0 : 45;
 
-            while (prob > 0.3f)
+            // Probability decay coefficient
+            var decay = hash.Float(0.1f, 0.96f, seed++);
+
+            for (var prob = 1.0f; prob > 0.3f;)
             {
-                var arm = diag ? math.float2(0.707f, 0.707f) : math.float2(1, 0);
-
                 for (var k = 0; k < 4; k++)
                 {
-                    var p1 = p0;
-                    p1.xy += arm * _nodeStride / 2;
+                    // Rotation
+                    angle += 90;
 
-                    if (rand.NextFloat() < prob)
-                        stack.Push(new Modeler()
-                          { Position = p1, Rotation = 90 * k + (diag ? 45 : 0), Shape = shape });
+                    // Probability test
+                    if (hash.Float(seed++) > prob) continue;
 
-                    arm = arm.yx * math.float2(-1, 1);
+                    // Node position
+                    var d = math.float2(_nodeStride * 0.5f, 0);
+                    d = math.mul(float2x2.Rotate(math.radians(angle)), d);
+                    var p = pos + math.float3(d, 0);
+
+                    // Modeler addition
+                    modeling.Push(new Modeler()
+                      { Position = p, Rotation = angle, Shape = board });
                 }
 
-                p0.z += _nodeStride;
-                prob *= rand.NextFloat(decay, 1.0f);
+                // Stride
+                pos.z += _nodeStride;
+
+                // Probability decay
+                prob *= hash.Float(decay, 1.0f, seed++);
             }
         }
 
-        GetComponent<MeshFilter>().sharedMesh = _mesh = MeshBuilder.Build(stack);
+        // Mesh building
+        _mesh = MeshBuilder.Build(modeling);
+        GetComponent<MeshFilter>().sharedMesh = _mesh;
     }
 
     void OnDestroy()
